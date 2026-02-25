858338bc345a286e9edf0cec67e96de4b1c0cbb8
ozan-san <ozan.san.98@gmail.com>
2026-02-25 18:04:20 +0300

    Revert "Test out the API call via Firebase"
    This reverts commit 2e3f4750a9c413c140944b53adc1bf6e56a3e5ca.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -54,6 +53,4 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.database)
 }
--- app/src/main/java/com/ozansan/hackernewsandroidclient/MainActivity.kt
+++ app/src/main/java/com/ozansan/hackernewsandroidclient/MainActivity.kt
@@ -11,36 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.firebase.Firebase
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.database
 import com.ozansan.hackernewsandroidclient.ui.theme.HackerNewsAndroidClientTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-
-        val database = Firebase.database("https://hacker-news.firebaseio.com")
-        val topStoriesRef = database.getReference("v0/topstories")
-
-        topStoriesRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val topStoryIds = snapshot.children.map { it.getValue(Long::class.java) }
-                // Now you have the IDs of the top stories
-                // You can then fetch the details of each story
-                println(topStoryIds)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                // Handle the error
-                println("Failed to read value." + error.toException())
-            }
-        })
-
         setContent {
             HackerNewsAndroidClientTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
